73401fd44cc6f8d141486c5c774c612f3e680418
Valeriy Morkovin <minouts@gmail.com>
2017-02-10 15:26:01 +0200

    version bumped: v0.3.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 
 setup(name='trafaret_validator',
-      version='0.3.5',
+      version='0.3.6',
       description='trafaret validator wrapper',
       long_description=read('README.rst'),
       classifiers=classifiers,
