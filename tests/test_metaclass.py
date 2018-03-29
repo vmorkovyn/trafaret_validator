04302adc7b78eecb53025b1feeba31767a65e4ce
Valeriy Morkovin <minouts@gmail.com>
2018-03-29 17:27:30 +0300

    add inheritance tests

--- tests/test_metaclass.py
+++ tests/test_metaclass.py
@@ -10,6 +10,10 @@ class ValidatorForTest(TrafaretValidator):
     value = 5
 
 
+class ValidatorForTest2(ValidatorForTest):
+    test = t.String()
+
+
 class TestMetaclass(TestCase):
 
     def test_metaclass(self):
@@ -24,4 +28,20 @@ class TestMetaclass(TestCase):
         self.assertFalse(ValidatorForTest._data,
                          '_data should be empty')
         self.assertFalse(ValidatorForTest._errors,
+                         '_data should be empty')
+
+    def test_inheritance(self):
+        self.assertIsInstance(ValidatorForTest2._validators, dict,
+                              'Value should be instance of dict')
+        self.assertIn('t_value', ValidatorForTest2._validators,
+                      'Value should be in _validators')
+        self.assertIn('test', ValidatorForTest2._validators,
+                      'Value should be in _validators')
+        self.assertNotIn('value', ValidatorForTest2._validators,
+                         'Value should not be in _validators')
+        self.assertIsInstance(ValidatorForTest2._trafaret, t.Trafaret,
+                              'Value should be instance of Trafaret')
+        self.assertFalse(ValidatorForTest2._data,
+                         '_data should be empty')
+        self.assertFalse(ValidatorForTest2._errors,
                          '_data should be empty')
