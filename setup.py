d4fb35026b81270cd44d1b58f0dadc8e179219f2
Valeriy Morkovyn <minouts@gmail.com>
2018-11-06 16:52:51 +0200

    add python 3.7 support

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Software Development :: Libraries',
     'Topic :: Utilities',
 ]
